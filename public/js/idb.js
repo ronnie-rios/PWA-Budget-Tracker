c4f49287f1e2056551b20eae856a2d9ddbd9fb9d
Ronnie Rios <ronniesmail14@gmail.com>
2021-07-31 14:20:20 -0500

    updated idb.js to handle offline functionality

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,6 +1,6 @@
 let db;
 
-const request= indexedDB.open('name', 1);
+const request= indexedDB.open('tracker', 1);
 
 request.onupgradeneeded = function(event) {
     const db = event.target.result;
@@ -9,9 +9,10 @@ request.onupgradeneeded = function(event) {
 
 //sucessful
 request.onsuccess = function(event) {
-    db = db.target.result;
+    db = event.target.result;
     if (navigator.onLine){
         //function to upload budget
+        uploadBudget();
     }
 }
 
@@ -20,3 +21,46 @@ request.onerror = function(event) {
 }
 
 // function saveTransact
+function saveRecord(record) {
+    const transaction = db.transaction(['new_budget'],'readwrite');
+    const budgetObjectStore = transaction.objectStore('new_budget');
+    budgetObjectStore.add(record);
+};
+
+function uploadBudget(){
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+    const budgetObjectStore = transaction.objectStore('new_budget');
+    const getAll = budgetObjectStore.getAll();
+
+
+getAll.onsuccess = function(){
+    if (getAll.result.length > 0) {
+        fetch('/api/transaction',{
+            method: 'POST',
+            body: JSON.stringify(getAll.result),
+            headers: {
+                Accept: 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(['new_budget'], 'readwrite');
+          // access the bduget object store
+          const budgetObjectStore = transaction.objectStore('new_budget');
+          // clear all items in your store
+          budgetObjectStore.clear();
+
+          alert('budget has been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    };
+    }
+}
+window.addEventListener('online', uploadBudget);
