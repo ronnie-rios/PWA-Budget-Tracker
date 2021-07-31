467f86e6e7c7001450f4f0b0f6e2c785d051b75f
Ronnie Rios <ronniesmail14@gmail.com>
2021-07-31 13:27:39 -0500

    updated service worker to include activate

--- service-worker.js
+++ service-worker.js
@@ -6,20 +6,49 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 
 
 
-self.addEventListener('install', function (e) {
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+      caches.match(e.request).then(function (request) {
+        if (request) { // if cache is available, respond with cache
+          console.log('responding with cache : ' + e.request.url)
+          return request
+        } else {       // if there are no cache, try fetching request
+          console.log('file is not cached, fetching : ' + e.request.url)
+          return fetch(e.request)
+        }
+      })
+    )
+  })
+  
+  // Cache resources
+  self.addEventListener('install', function (e) {
     e.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-          console.log('installing cache : ' + CACHE_NAME)
-          return cache.addAll(FILES_TO_CACHE)
+      caches.open(CACHE_NAME).then(function (cache) {
+        console.log('installing cache : ' + CACHE_NAME)
+        return cache.addAll(FILES_TO_CACHE)
+      })
+    )
+  })
+  
+  // Delete outdated caches
+  self.addEventListener('activate', function (e) {
+    e.waitUntil(
+      caches.keys().then(function (keyList) {
+       
+        let cacheKeeplist = keyList.filter(function (key) {
+          return key.indexOf(APP_PREFIX);
         })
-      )
-});
-caches.match(e.request).then(function (request) {
-    if (request) {
-      console.log('responding with cache : ' + e.request.url)
-      return request
-    } else {
-      console.log('file is not cached, fetching : ' + e.request.url)
-      return fetch(e.request)
-    }
-})
+        // add current cache name to keeplist
+        cacheKeeplist.push(CACHE_NAME);
+  
+        return Promise.all(keyList.map(function (key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log('deleting cache : ' + keyList[i] );
+            return caches.delete(keyList[i]);
+          }
+        }));
+      })
+    );
+  });
