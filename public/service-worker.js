d16f4d180d5f4d6f2232c331d83910ec4bd55c44
Ronnie Rios <ronniesmail14@gmail.com>
2021-07-31 14:30:15 -0500

    added files to cache and changed icons arr

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,27 +1,22 @@
-const FILES_TO_CACHE=[];
+const FILES_TO_CACHE=[
+  "/index.html",
+  "./js/index.js",
+  "./css/style.css",
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png",
+  "/manifest.json"
+];
 
 const APP_PREFIX = 'Budget Tracker-';
 const VERSION= 'verson_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
-
-
-// Respond with cached resources
-self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-      caches.match(e.request).then(function (request) {
-        if (request) { // if cache is available, respond with cache
-          console.log('responding with cache : ' + e.request.url)
-          return request
-        } else {       // if there are no cache, try fetching request
-          console.log('file is not cached, fetching : ' + e.request.url)
-          return fetch(e.request)
-        }
-      })
-    )
-  })
-  
   // Cache resources
   self.addEventListener('install', function (e) {
     e.waitUntil(
@@ -52,3 +47,18 @@ self.addEventListener('fetch', function (e) {
       })
     );
   });
+  // Respond with cached resources
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+      caches.match(e.request).then(function (request) {
+        if (request) { // if cache is available, respond with cache
+          console.log('responding with cache : ' + e.request.url)
+          return request
+        } else {       // if there are no cache, try fetching request
+          console.log('file is not cached, fetching : ' + e.request.url)
+          return fetch(e.request)
+        }
+      })
+    )
+  })
